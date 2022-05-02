2256ea77e5e3cce6737a2cea7e1f693dee494176
Yash Kumar <w0749495@myscc.ca>
2022-05-02 19:08:39 -0400

    UI and functionality done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+void playSound(int soundNummber){
+  final player = AudioCache();
+  player.play('note$soundNummber.wav');
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              children: [
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.red,
+                    onPressed: (){
+                      playSound(1);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.orange,
+                    onPressed: (){
+                      playSound(2);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.yellow,
+                    onPressed: (){
+                      playSound(3);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.green,
+                    onPressed: (){
+                      playSound(4);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.greenAccent,
+                    onPressed: (){
+                      playSound(5);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: (){
+                      playSound(6);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.purple,
+                    onPressed: (){
+                      playSound(7);
+                    },
+                    minWidth: double.infinity,
+                  ),
+                ),
+              ],
+            ),
+            color: Colors.black,
+            width: double.infinity,
+          ),
+
         ),
+        backgroundColor: Colors.black,
       ),
     );
   }
