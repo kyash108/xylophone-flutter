53539331ab705692d091e71a8f6650e67d1f56c0
Yash Kumar <w0749495@myscc.ca>
2022-05-02 20:13:12 -0400

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _XylophoneAppState extends State<XylophoneApp> {
                 buildKey(color: Colors.orange,sound:2),
                 buildKey(color: Colors.yellow,sound:3),
                 buildKey(color: Colors.green,sound:4),
-                buildKey(color: Colors.greenAccent,sound:5),
+                buildKey(color: Colors.teal,sound:5),
                 buildKey(color: Colors.blue,sound:6),
                 buildKey(color: Colors.purple,sound:7),
               ],
